40bd8957a9941b56fbdcd7f1a23de6b4377fd82e
bed-com-18-19 <bed-com-18-19@unima.ac.mw>
2022-01-23 13:05:35 -0800

    1st commit

---
+++ numberArray.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace numberArray
+{
+    // main class 
+    public class NumberArray
+    {
+
+        // main method 
+        public static void Main(String [] args)
+        {
+            // main method body
+            // 
+            int [] elements = {12, 4, 6,12, 45,67,78};
+
+            // printing an element at particular index
+            Console.WriteLine("The number is " + elements[0]);
+        }
+    }
+}
